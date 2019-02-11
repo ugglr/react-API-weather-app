b41edbb35f7e966b135cf5a0af4c3b327f8c21b9
CarlW <carl@oskarroger.net>
2019-02-11 21:00:22 +0800

    added props function to Forms. API call works

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "35a73ac3082a5af11e4e9bc2aa2a4e26";
+
 class App extends React.Component {
+  getWeather = async e => {
+    e.preventDefault();
+    const api_call = await fetch(
+      `api.openweathermap.org/data/2.5/weather?
+      q=London,
+      uk&APPID=${API_KEY}`
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
+
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
