62935c121fd7286cff2212e6bb2834bf85a580cb
CarlW <carl@oskarroger.net>
2019-02-11 17:54:04 +0800

    Rendered the App component to the DOM, everything is working

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello!</p>
+        <h1>YO!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
