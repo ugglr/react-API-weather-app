3cd10919b345f86f31c89c50130ab82deda698f0
CarlW <carl@oskarroger.net>
2019-02-12 18:16:30 +0800

    Handles undefined states and prompts the user to enter values.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,9 +4,15 @@ class Weather extends React.Component {
   render() {
     return (
       <div>
-        <p>This will show the temperature: </p>
-        {this.props.city}
-        {this.props.temperature}
+        {this.props.city && this.props.country && (
+          <p>
+            Location: {this.props.city}, {this.props.country}
+          </p>
+        )}
+        {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+        {this.props.description && <p>Conditions: {this.props.description}</p>}
+        {this.props.error && <p>{this.props.error}</p>}
       </div>
     );
   }
